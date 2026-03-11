5b5bf0dcf965acc1f741bdba0d7f86a48d9626f0
Ome Tiwari <omt81144@gmail.com>
2026-03-11 22:50:05 +0530

    backend folder

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,6 +4,7 @@ const authRouter = Router();
 // const authRouter = express.Router();
 
 const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware")
 
 /**
  * @route POST /api/auth/register
@@ -19,5 +20,20 @@ authRouter.post("/register",authController.registerUserController )
  */
 authRouter.post("/login", authController.loginUserController)
 
+
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
+ */
+authRouter.get("/logout", authController.logoutUserController)
+
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController)
 
 module.exports = authRouter;
